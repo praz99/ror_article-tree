5cf2875cbc061df2f5b87bb34b47dd681e09a463
praz <t.prazwal@gmail.com>
2020-09-22 19:56:25 +0200

    Create the basic authentication Add sessions_controller.rb file for login/logout Add session_helper.rb file Add helper methods in session_helper.rb file Add login/logout links in the navbar Add routes for login/logout Add index.html.erb file for showing all articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :logged_in_user, only: [:show]
+
   def new
     @user = User.new
   end
@@ -6,11 +8,16 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      log_in @user
       flash[:success] = 'Welcome to Articles Tree!'
       redirect_to @user
     else
       render 'new'
     end
+  end
+
+  def show
+    @user = User.find(params[:id])
   end
 
   private
--- app/models/article.rb
+++ app/models/article.rb
@@ -1,3 +1,4 @@
 class Article < ApplicationRecord
-  belongs_to :author
+  belongs_to :author, class_name: 'User'
+  validates :title, presence: true
 end
