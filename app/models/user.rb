22dd8c9e0e8378f746eaacf70e6865ce6fc8c14e
praz <t.prazwal@gmail.com>
2020-09-22 16:59:19 +0200

    Add controller methods for user model Add new, create and show methods inside UsersController Add form for creating new user Add view for showing user page Add navbar for showing sign up links Add view for showing errors

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,4 @@
 class User < ApplicationRecord
+  has_many :authored_articles, foreign_key: 'author_id', class_name: 'Article'
+  validates :name, presence: true, uniqueness: true, length: { in: 3..30 }
 end
