80e4776c3d279792744e0cc605d36adae766ceff
praz <t.prazwal@gmail.com>
2020-09-23 14:18:11 +0200

    Add methods to create and show new articles Add new.html.erb file to create new articles, Add show.html.erb file to show an article, Modify index.html.erb file to show list of all articles, Modify articles_controller.rb to add methods for create and show articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,2 +1,56 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: %i[show edit update destroy]
+  before_action :logged_in_user, only: %i[new upddate destroy]
+  def index
+    @articles = Article.all.order('created_at DESC')
+  end
+
+  def new
+    @article = current_user.authored_articles.build
+  end
+
+  def create
+    @article = current_user.authored_articles.build(article_params)
+    if @article.save
+      flash[:success] = 'Article Created!'
+      redirect_to @article
+    else
+      flash[:danger] = 'Something went wrong'
+      render 'new'
+    end
+  end
+
+  def show; end
+
+  def edit; end
+
+  def update
+    if @article.update(article_params)
+      flash[:success] = 'Article edit success!'
+      redirect_to @article
+    else
+      flash[:danger] = 'Something went wrong'
+      render 'edit'
+    end
+  end
+
+  def destroy
+    if @article.destroy
+      flash[:success] = 'Article delete success!'
+      redirect_to articles_path
+    else
+      flash[:danger] = 'Something went wrong!'
+      redirect_to @article
+    end
+  end
+
+  private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :text)
+  end
 end
