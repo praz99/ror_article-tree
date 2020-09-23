68f141e63f111c131c37d7255cff3c2b31c98dfe
praz <t.prazwal@gmail.com>
2020-09-23 19:42:49 +0200

    Add validations for uploading images

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,4 +2,18 @@ class Article < ApplicationRecord
   belongs_to :author, class_name: 'User'
   validates :title, presence: true
   has_one_attached :image
+  validate :accepted_image
+
+  def accepted_image
+    return unless image.attached?
+
+    unless image.byte_size <= 2.megabyte
+      errors.add(:image, 'is too large (max 2MB)')
+    end
+
+    accepted_types = ['image/jpeg', 'image/jpg', 'image/png']
+    unless accepted_types.include?(image.content_type)
+      errors.add(:image, 'must be a JPEG/JPG or PNG')
+    end
+  end
 end
