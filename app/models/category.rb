79e415e27055c73b33c151fced991cbd8307b9cb
praz <t.prazwal@gmail.com>
2020-09-27 15:18:32 +0200

    Add validations for category model

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,6 +2,9 @@ class Category < ApplicationRecord
   has_many :categorizes
   has_many :articles, through: :categorizes
 
+  validates :name, presence: true, uniqueness: true, length: { in: 3..30 }
+  validates :priority, numericality: { only_integer: true }
+
   def self.ordered_priority
     all.includes(:articles).order('priority DESC')
   end
