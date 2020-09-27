d4922e40804f26b1fc8c125e7af9e514516311f1
praz <t.prazwal@gmail.com>
2020-09-27 11:17:45 +0200

    style the signup page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:show]
+  before_action :logged_in_user, only: [:index, :show]
+
+  def index; end
 
   def new
     @user = User.new
