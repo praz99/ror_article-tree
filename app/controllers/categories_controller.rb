94b12a243d6ee0dd39deb2bfa110a247fa3cf0c2
praz <t.prazwal@gmail.com>
2020-09-27 22:10:39 +0200

    fix stylelint errors

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,5 +1,5 @@
 class CategoriesController < ApplicationController
-  before_action :logged_in_user, except: [:home, :show, :index]
+  before_action :logged_in_user, except: %i[home show index]
   before_action :set_category, only: %I[show edit upddate destroy]
 
   def index
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:index, :show]
+  before_action :logged_in_user, only: %i[index show]
 
   def index; end
 
