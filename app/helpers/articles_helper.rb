3b722e7433aca8aa65863b4574ebee5a82912b0b
praz <t.prazwal@gmail.com>
2020-09-28 02:01:17 +0200

    remove logic from views

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -1,2 +1,22 @@
 module ArticlesHelper
+  def form_image_check(form)
+    if @article.image.attached?
+      render 'articles/partials/has_image', f: form
+    else
+      render 'articles/partials/has_no_image', f: form
+    end
+  end
+
+  def show_image_check
+    render 'articles/partials/show_image' if @article.image.attached?
+  end
+
+  def show_vote(var)
+    vote = Vote.find_by(article: var, user: current_user)
+    if vote
+      render 'articles/partials/votedown', var: var, vote: vote
+    else
+      render 'articles/partials/voteup', var: var
+    end
+  end
 end
--- app/helpers/categories_helper.rb
+++ app/helpers/categories_helper.rb
@@ -1,2 +1,21 @@
 module CategoriesHelper
+  def background_check(var)
+    url_for(var.articles.last.image) if var.articles.exists?
+  end
+
+  def home_title_check(var)
+    if var.articles.exists?
+      render 'categories/partials/has_title', cat: var
+    else
+      content_tag(:p, 'no articles', class: ['index-category-articles-count'])
+    end
+  end
+
+  def article_count_check(var)
+    if var.articles.exists?
+      render 'categories/partials/has_article', category: var
+    else
+      content_tag(:p, 'no articles', class: ['index-category-articles-count'])
+    end
+  end
 end
