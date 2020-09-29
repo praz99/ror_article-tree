973ec2bb7d6210b814b472da6fc3f58c25fc8f82
praz <t.prazwal@gmail.com>
2020-09-29 02:31:53 +0200

    add test for login feature

--- spec/features/login_spec.rb
+++ spec/features/login_spec.rb
@@ -10,13 +10,21 @@ describe 'login', type: :feature do
     end
     expect(page).to have_content('Welcome to Articles Tree!')
     click_on 'log out'
+    expect(page).to have_content('Looks like you are the first user or there are currently no any articles.')
   end
 
   scenario 'valid inputs' do
     visit '/login'
-    expect(page).to have_content('Registger Now')
-    fill_in 'session_name', with: @user.name
+    expect(page).to have_content('Username')
+    fill_in 'session_name', with: 'alice'
     click_on(class: 'new-session-button')
     expect(page).to have_content('Hello alice! Welcome back.')
+  end
+
+  scenario 'invalid inputs' do
+    visit '/login'
+    expect(page).to have_content('Username')
+    fill_in 'session_name', with: 'Hello There'
+    expect(page).to have_content('Username')
   end
 end
