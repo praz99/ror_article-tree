930816916857c7f359188b3276027882cc74e8db
praz <t.prazwal@gmail.com>
2020-09-29 11:26:24 +0200

    Add test for CRUD options test category create feature test article create feature

--- spec/features/authentication_spec.rb
+++ spec/features/authentication_spec.rb
@@ -28,3 +28,20 @@ RSpec.describe 'login', type: :feature do
     expect(page).to have_content('Username')
   end
 end
+
+RSpec.describe 'logout', type: :feature do
+  before(:example) do
+    visit signup_path
+    expect(page).to have_content('Enter a unique username')
+    fill_in 'user_name', with: 'alice'
+    within('.new-user-btn') do
+      click_on 'Register'
+    end
+    expect(page).to have_content('Welcome to Articles Tree!')
+  end
+
+  scenario 'logout user' do
+    click_on 'log out'
+    expect(page).to have_content('Looks like you are the first user or there are currently no any articles.')
+  end
+end
